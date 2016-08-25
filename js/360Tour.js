1da00fd8a2eff62e26251d13a424b0071db6c1c9
Josh Harle <josh.harle@gmail.com>
2016-08-25 16:56:23 +1000

    Google analytic tracking for things #26

--- js/360Tour.js
+++ js/360Tour.js
@@ -72,6 +72,9 @@ $(document).ready(function () {
 	time = getParameterByName("time") ? getParameterByName("time") : 0;
 	fov = getParameterByName("fov") ? getParameterByName("fov") : 90;
 	
+	// start tracking
+	initTracking();
+	
 	// preload share image
 	preload(['images/share-icons.png']);
 	
@@ -81,6 +84,16 @@ $(document).ready(function () {
 	  callPano("endShare();");
 	});
 });
+
+function initTracking() {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1226844-19', 'auto');
+  ga('send', 'pageview');
+}
 
 function preload(arrayOfImages) {
     $(arrayOfImages).each(function(){
@@ -96,6 +109,11 @@ function hideStartBox() {
 
 function showShare() {
 	$( "#share" ).fadeIn(200);
+	ga('send', 'event', {
+		eventCategory: 'Tour',
+		eventAction: 'share',
+		eventLabel: location.href
+	});
 }
 
 function startWithParameters(v) {
